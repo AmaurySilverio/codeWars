7e8e6ef70ac4a7f069f2d8371fe3bdeee6e8951c
AmaurySilverio <amaurycodes@gmail.com>
2023-03-24 15:40:07 -0400

    codewars day 10 part three

---
+++ day10PartThreeConvertANumberToAString.js
@@ -0,0 +1,7 @@
+// DAY 10 Part Three
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+const numberToString = (num) => num.toString();
