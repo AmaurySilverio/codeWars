5f81e6c674a7ddabb0a1efc16cbd329b0200e769
AmaurySilverio <amaurycodes@gmail.com>
2023-05-15 23:40:27 -0400

    day 42 codewars make acronym

---
+++ day42makeAcronym.js
@@ -0,0 +1,20 @@
+//day42
+//7 Kyu
+
+// Write function which takes a string and make an acronym of it.
+
+// Rule of making acronym in this kata:
+
+// split string to words by space char
+// take every first letter from word in given string
+// uppercase it
+// join them toghether
+// Eg:
+
+// Code wars -> C, w -> C W -> CW
+// Note: There will be at least two words in the given string!
+
+function toAcronym(inp) {
+  let str = inp.split(" ").map((word) => word[0].toUpperCase());
+  return str.join("");
+}
