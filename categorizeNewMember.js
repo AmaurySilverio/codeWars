2aafc7b2e76154dc2d421851931f63f0ca4c9580
AmaurySilverio <amaurycodes@gmail.com>
2023-10-12 21:51:23 -0400

    Categorize New Member 7Kyu Codewars

---
+++ categorizeNewMember.js
@@ -0,0 +1,36 @@
+//7 Kyu
+
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+// Input
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+// Output
+// Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+// Example
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+//My answer
+function openOrSenior(data) {
+  let answer = [];
+  for (let i = 0; i < data.length; i++) {
+    let potentialMember = data[i];
+    if (potentialMember[0] >= 55 && potentialMember[1] > 7) {
+      answer.push("Senior");
+    } else {
+      answer.push("Open");
+    }
+  }
+  return answer;
+}
+
+//Answer I like
+function openOrSenior(data) {
+  return data.map(([age, handicap]) =>
+    age > 54 && handicap > 7 ? "Senior" : "Open"
+  );
+}
